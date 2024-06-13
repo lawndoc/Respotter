fda8962614c1f54461ad01f0874d81c1481af080
C.J. May <c.j.may@vermeer.com>
2024-06-13 12:15:25 -0500

    skeleton for python rewrite

---
+++ respotter.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import json
+import scapy
+
+class Respotter:
+    def __init__(self):
+        pass
+    
+    def run(self):
+        pass
+
+if __name__ == "__main__":
+    pass
