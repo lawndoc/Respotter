303b4aa9593841a7783711b6e370953d8147b6fa
C.J. May <c.j.may@vermeer.com>
2024-06-13 13:16:07 -0500

    create llmnr request functionality

--- respotter.py
+++ respotter.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python3
 
 import json
-import scapy
+from scapy.all import *
+from scapy.layers.dns import DNS, DNSQR
+from scapy.layers.inet import IP, UDP
+from time import sleep
 
 class Respotter:
-    def __init__(self):
-        pass
+    def __init__(self, delay=30, hostname="Loremipsumdolorsitamet"):
+        self.delay = delay
+        self.hostname = hostname
+    
+    def send_llmnr_request(self):
+        # LLMNR uses the multicast IP 224.0.0.252 and UDP port 5355
+        packet = IP(dst="224.0.0.252")/UDP(dport=5355)/DNS(rd=1, qd=DNSQR(qname=self.hostname))
+        send(packet)
     
     def run(self):
-        pass
+        while True:
+            self.send_llmnr_request()
+            sleep(self.delay)
 
 if __name__ == "__main__":
-    pass
+    respotter = Respotter(delay=3)
+    respotter.run()
