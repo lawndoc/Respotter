5e689c5b602acbd73c3809a822206d68530cb391
C.J. May <c.j.may@vermeer.com>
2024-06-14 17:17:06 -0500

    rename run to daemon and add more init args

--- respotter.py
+++ respotter.py
@@ -8,14 +8,20 @@ from scapy.layers.netbios import NBNSQueryRequest, NBNSQueryResponse
 from time import sleep
 
 class Respotter:
-    def __init__(self, delay=30, hostname="Loremipsumdolorsitamet"):
+    def __init__(self,
+                 delay=30,
+                 hostname="Loremipsumdolorsitamet",
+                 timeout=3,
+                 verbosity=0):
         self.delay = delay
         self.hostname = hostname
+        self.timeout = timeout
+        self.verbosity = verbosity
     
     def send_llmnr_request(self):
         # LLMNR uses the multicast IP 224.0.0.252 and UDP port 5355
         packet = IP(dst="224.0.0.252")/UDP(dport=5355)/LLMNRQuery(qd=DNSQR(qname=self.hostname))
-        response = sr1(packet, timeout=3, verbose=0)
+        response = sr1(packet, timeout=self.timeout, verbose=self.verbosity)
         if response is not None and response.haslayer(DNS):
             # Print all resolved IP addresses
             for i in range(response[LLMNRResponse].ancount):
@@ -25,7 +31,7 @@ class Respotter:
     def send_mdns_request(self):
         # mDNS uses the multicast IP 224.0.0.251 and UDP port 5353
         packet = IP(dst="224.0.0.251")/UDP(dport=5353)/DNS(rd=1, qd=DNSQR(qname=self.hostname))
-        response = sr1(packet, timeout=3, verbose=0)
+        response = sr1(packet, timeout=self.timeout, verbose=self.verbosity)
         if response is not None and response.haslayer(DNS):
             # Print all resolved IP addresses
             for i in range(response[DNS].ancount):
@@ -35,13 +41,13 @@ class Respotter:
     def send_nbns_request(self):
         # NBNS uses the broadcast IP 255.255.255.255 and UDP port 137
         packet = IP(dst="255.255.255.255")/UDP(sport=137, dport=137)/NBNSQueryRequest(SUFFIX="file server service", QUESTION_NAME=self.hostname, QUESTION_TYPE="NB")
-        response = sr1(packet, timeout=3, verbose=0)
+        response = sr1(packet, timeout=self.timeout, verbose=self.verbosity)
         if response is not None and response.haslayer(NBNSQueryResponse):
             # Print all resolved IP addresses
             for i in range(response[NBNSQueryResponse].RDLENGTH):
                 print(f"!!! Responder detected at: {response[NBNSQueryResponse].ADDR_ENTRY[i].NB_ADDRESS}")
     
-    def run(self):
+    def daemon(self):
         while True:
             self.send_llmnr_request()
             self.send_mdns_request()
@@ -49,5 +55,5 @@ class Respotter:
             sleep(self.delay)
 
 if __name__ == "__main__":
-    respotter = Respotter(delay=3)
-    respotter.run()
+    respotter = Respotter(delay=0)
+    respotter.daemon()
