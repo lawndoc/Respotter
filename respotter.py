bc9eb42952a0c1ae5a5aa2c7b7e7a47f4d7b8352
C.J. May <c.j.may@vermeer.com>
2024-06-26 13:45:07 -0500

    try async sniffer

--- respotter.py
+++ respotter.py
@@ -5,8 +5,6 @@ from scapy.layers.dns import DNS, DNSQR
 from scapy.layers.inet import IP, UDP
 from scapy.layers.llmnr import LLMNRQuery, LLMNRResponse
 from scapy.layers.netbios import NBNSQueryRequest, NBNSQueryResponse, NBNSHeader
-from scapy.supersocket import SuperSocket
-from select import select
 from socket import *
 from time import sleep
 from optparse import OptionParser
@@ -34,7 +32,12 @@ class Respotter:
     def send_llmnr_request(self):
         # LLMNR uses the multicast IP 224.0.0.252 and UDP port 5355
         packet = IP(dst="224.0.0.252")/UDP(dport=5355)/LLMNRQuery(qd=DNSQR(qname=self.hostname))
-        response = sr1(packet, timeout=self.timeout, verbose=self.verbosity)
+        sniffer = AsyncSniffer(filter="udp dst port 5355", store=True)
+        sniffer.start()
+        sr1(packet, timeout=self.timeout, verbose=self.verbosity)
+        time.sleep(self.timeout)
+        sniffer.stop()
+        response = sniffer.results
         if response is not None and response.haslayer(LLMNRResponse):
             # Print all resolved IP addresses
             for i in range(response[LLMNRResponse].ancount):
@@ -44,7 +47,12 @@ class Respotter:
     def send_mdns_request(self):
         # mDNS uses the multicast IP 224.0.0.251 and UDP port 5353
         packet = IP(dst="224.0.0.251")/UDP(dport=5353)/DNS(rd=1, qd=DNSQR(qname=self.hostname))
-        response = sr1(packet, timeout=self.timeout, verbose=self.verbosity)
+        sniffer = AsyncSniffer(filter="udp dst port 5353", store=True)
+        sniffer.start()
+        sr1(packet, timeout=self.timeout, verbose=self.verbosity)
+        time.sleep(self.timeout)
+        sniffer.stop()
+        response = sniffer.results
         if response is not None and response.haslayer(DNS):
             # Print all resolved IP addresses
             for i in range(response[DNS].ancount):
@@ -54,7 +62,12 @@ class Respotter:
     def send_nbns_request(self):
         # NBNS uses the broadcast IP 255.255.255.255 and UDP port 137
         packet = IP(dst="255.255.255.255")/UDP(sport=137, dport=137)/NBNSHeader(OPCODE=0x0, NM_FLAGS=0x11, QDCOUNT=1)/NBNSQueryRequest(SUFFIX="file server service", QUESTION_NAME=self.hostname, QUESTION_TYPE="NB")
-        response = sr1(packet, timeout=self.timeout, verbose=self.verbosity)
+        sniffer = AsyncSniffer(filter="udp dst port 137", store=True)
+        sniffer.start()
+        sr1(packet, timeout=self.timeout, verbose=self.verbosity)
+        time.sleep(self.timeout)
+        sniffer.stop()
+        response = sniffer.results
         if response is not None and response.haslayer(NBNSQueryResponse):
             # Print all resolved IP addresses
             for i in range(response[NBNSQueryResponse].RDLENGTH):
