7aaeb3bc557ca206acf17bafa111a7911138cc01
xmjp <xmjp03@gmail.com>
2024-07-17 12:01:18 -0500

    fixed error handling

--- utils/slack.py
+++ utils/slack.py
@@ -4,16 +4,20 @@ import time
 
 def send_slack_message(webhook_url, title, details):
     client = WebhookClient(webhook_url)
-    response = client.send(
-        text=f"{title}\n{details}"
-    )
-    if response.status_code == 200:
-        print("Message sent successfully")
-    if response.status_code == 429:
-        # Slack rate limits to one message per channel per second, with short bursts of >1 allowed
-        retry_after = int(e.response.headers['Retry-After'])
-        print(f"Rate limited. Retrying in {retry_after} seconds")
-        time.sleep(retry_after)
-        send_slack_message(webhook_url, title, details)
-    else :
-        print(f"Failed to send message: {e.response.status_code}")
+    try:
+        response = client.send(
+            text=f"{title}\n{details}"
+        )
+        if response.status_code == 200:
+            print("Message sent successfully")
+    except SlackApiError as e:        
+        if e.response.status_code == 429:
+            # Slack rate limits to one message per channel per second, with short bursts of >1 allowed
+            retry_after = int(e.response.headers['Retry-After'])
+            print(f"Rate limited. Retrying in {retry_after} seconds")
+            time.sleep(retry_after)
+            response = client.send(
+                text=f"{title}\n{details}"
+                )   
+        else :
+            print(f"Failed to send message: {e.response.status_code}")
