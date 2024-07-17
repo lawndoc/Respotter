ade06b077f8ea70e1c6fba89c6d8c1410d23ba82
C.J. May <lawndoc@protonmail.com>
2024-07-17 18:16:31 +0000

    remove author

--- utils/discord.py
+++ utils/discord.py
@@ -4,7 +4,6 @@ from utils.errors import WebhookException
 def send_discord_message(webhook_url, title, details):
     webhook = DiscordWebhook(url=webhook_url, rate_limit_retry=True)
     embed = DiscordEmbed(title=title, description=details, color=242424)
-    embed.set_author(name='Respotter')
     embed.set_thumbnail(url='https://raw.githubusercontent.com/lawndoc/Respotter/main/assets/respotter_logo.png')
     webhook.add_embed(embed)
     response = webhook.execute()
